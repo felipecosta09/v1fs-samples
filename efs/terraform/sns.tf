1b257d1f9f15457c07e564d5ff327721b1bc47bf
Felipe Costa <33869171+felipecosta09@users.noreply.github.com>
2023-06-23 16:49:20 -0500

    add schedule scan

--- efs/terraform/sns.tf
+++ efs/terraform/sns.tf
@@ -1,5 +1,6 @@
 resource "aws_sns_topic" "sns_topic" {
   name = "${var.prefix}-topic-${random_string.random.id}"
+  display_name = "${var.prefix}-topic"
   tags = {
     Name = "${var.prefix}-topic"
   }
